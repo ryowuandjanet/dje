f09bdd4249c4c06d5cf61e4e3d986d77972a8afa
ryowuandjanet <ryowu0329@gmail.com>
2023-09-05 22:38:55 +0800

    Part 04

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -125,6 +125,7 @@ STATIC_URL = 'static/'
 
 MEDIA_URL = 'images/'
 MEDIA_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [ BASE_DIR / 'static' ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- store/views.py
+++ store/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from .models import *
 
 def home(request):
   return render(request,"store/index.html")
+
+def collections(request):
+  category = Category.objects.filter(status=0)
+  context = {'category': category}
+  return render(request, "store/collections.html", context)
