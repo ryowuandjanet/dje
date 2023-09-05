bce3904a3cd8d93aeb79d8336c2d1489670e775d
ryowuandjanet <ryowu0329@gmail.com>
2023-09-05 23:24:31 +0800

    Part 05

--- store/views.py
+++ store/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
 from .models import *
 
 def home(request):
@@ -8,3 +9,13 @@ def collections(request):
   category = Category.objects.filter(status=0)
   context = {'category': category}
   return render(request, "store/collections.html", context)
+
+def collectionsview(request,slug):
+  if(Category.objects.filter(slug=slug, status=0)):
+    products = Product.objects.filter(category__slug=slug)
+    category = Category.objects.filter(slug=slug).first()
+    context = {'products': products, "category": category}
+    return render(request, 'store/products/index.html',context)
+  else:
+    messages.warning(request,"No Such category found")
+    return redirect("collections")
