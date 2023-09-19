079529be7b238f4514beeae5448e3c294a413231
ryowuandjanet <ryowu0329@gmail.com>
2023-09-20 01:53:39 +0800

    modify allowed_hosts

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-^khj%e&(4m(e+#d*5_!33ctscw(**7h#8h1je7s%jo^p*dxfb%
 DEBUG = True
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['.vercel.app','.now.sh','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['134.209.220.251','.vercel.app','.now.sh','127.0.0.1','localhost']
 
 INSTALLED_APPS = [
     'jazzmin',
